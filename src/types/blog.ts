25b5eee062f5838553c9452d8c64b4520e2d0456
UKouji555 <zerozero53205320.dev@gmail.com>
2025-08-08 16:26:24 +0900

    git commit -m "feat: blog abount practice vim add"

--- src/types/blog.ts
+++ src/types/blog.ts
@@ -3,20 +3,13 @@ export interface BlogPost {
   title: string;
   excerpt: string;
   content?: string;
-  author: string;
-  authorAvatar: string;
   date: string;
-  readTime: string;
   tags: string[];
   imageUrl: string;
-  likes: number;
-  comments: number;
 }
 
 export interface Comment {
   id: number;
-  author: string;
-  avatar: string;
   content: string;
   timestamp: string;
 }
