9504ec5886bf5c94f33aac30e136bd30dd3be841
UKouji555 <zerozero53205320.dev@gmail.com>
2025-11-08 22:55:36 +0900

    fix: Fixing GitHub Action Errors

--- src/components/Hello.test.tsx
+++
@@ -1,7 +0,0 @@
-import {render} from '@testing-library/react';
-import {Hello} from './Hello';
-
-test('renders greeting', () => {
-  const {container} = render(<Hello name="React"/>);
-  expect(container.textContent).toContain('Hello React');
-});
--- src/components/Hello.tsx
+++
@@ -1,3 +0,0 @@
-export function Hello({ name }: { name: string }) {
-  return <h1>Hello {name}</h1>;
-}
